5eb5aa3a86fbe0567dc553ce67789e19fec1b5b6
Luis Vegas <luisvegasmor@gmail.com>
2025-10-13 04:03:14 -0400

    feat: added some basic distributions for exploratory purposes

--- project.r
+++ project.r
@@ -30,3 +30,29 @@ gaming_clean <- gaming_clean %>%
   filter(Hours <= 100)
 
 write_csv(gaming_clean, "GamingStudy_clean.csv")
+#View(gaming_clean)
+
+summary(gaming_clean)
+
+describe(gaming_clean %>% select(Age, Hours, GAD_T, SPIN_T, SWL_T, Narcissism))
+
+# horas jugadas
+ggplot(gaming_clean, aes(x = Hours)) +
+  geom_histogram(fill = "steelblue", bins = 30, alpha = 0.7) +
+  theme_minimal() +
+  labs(title = "Distribución de horas de juego semanales",
+       x = "Horas jugadas por semana", y = "Frecuencia")
+
+# ansiedad (GAD)
+ggplot(gaming_clean, aes(x = GAD_T)) +
+  geom_histogram(fill = "orange", bins = 30, alpha = 0.7) +
+  theme_minimal() +
+  labs(title = "Distribución de ansiedad (GAD_T)",
+       x = "Puntaje total GAD", y = "Frecuencia")
+
+# aatisfaccion con la vida
+ggplot(gaming_clean, aes(x = SWL_T)) +
+  geom_histogram(fill = "darkgreen", bins = 30, alpha = 0.7) +
+  theme_minimal() +
+  labs(title = "Distribución de satisfacción con la vida (SWL_T)",
+       x = "Puntaje SWL_T", y = "Frecuencia")
